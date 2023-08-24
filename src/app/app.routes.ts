559828e17a33838af5234f13fb196984f3714847
Outlander <jo.castillog@duocuc.cl>
2023-08-24 19:58:02 -0400

    agregando login, user page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,15 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'user',
+    loadComponent: () => import('./user/user.page').then( m => m.UserPage)
   },
 ];
