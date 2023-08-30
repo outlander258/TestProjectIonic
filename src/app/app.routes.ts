50d695519903b96f1a060cf3d53e494f0a918462
Outlander <jo.castillog@duocuc.cl>
2023-08-29 23:05:19 -0400

    borrando home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
+
   {
     path: '',
     redirectTo: 'login',
