2bae79cea8701fdc7ef1901e1a8778db0f924db3
Outlander <jo.castillog@duocuc.cl>
2023-08-29 23:27:42 -0400

    Resuelve conflictos de fusión en app.routes.ts2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
+
   {
     path: '',
     redirectTo: 'user',
@@ -17,7 +14,8 @@ export const routes: Routes = [
   {
     path: 'user',
     loadComponent: () => import('./user/user.page').then( m => m.UserPage)
-  },  {
+  },
+  {
     path: 'docente',
     loadComponent: () => import('./docente/docente.page').then( m => m.DocentePage)
   },
@@ -26,23 +24,5 @@ export const routes: Routes = [
     loadComponent: () => import('./password/password.page').then( m => m.PasswordPage)
   },
 
-<<<<<<< HEAD
-export const routes: Routes = [
 
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  },
-  {
-    path: 'login',
-    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
-  },
-  {
-    path: 'user',
-    loadComponent: () => import('./user/user.page').then( m => m.UserPage)
-  },
 ];
-=======
-];
->>>>>>> 5c84e98ef2f7f2addae09f492a25c12c35b3e5f4
