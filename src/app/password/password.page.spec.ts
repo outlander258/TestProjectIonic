61f5be6b9813603f2b7dd988233f12f107cf59dc
Danilo Jara <dani.jaram@duocuc.cl>
2023-08-29 21:42:23 -0400

    Password Vacio

---
+++ src/app/password/password.page.spec.ts
@@ -0,0 +1,17 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PasswordPage } from './password.page';
+
+describe('PasswordPage', () => {
+  let component: PasswordPage;
+  let fixture: ComponentFixture<PasswordPage>;
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(PasswordPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
